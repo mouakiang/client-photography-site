f5468c51fa0ae1ece83ae6172483d3a55aaa7626
Kia Moua <116847348+mouakiang@users.noreply.github.com>
2023-12-04 17:20:32 -0500

    added text styling, button and styling

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import "./home.css";
+import homeImg from '../../assets/02.jpg';
 
 export default function Home() {
   return (
     <div className="home">
-     
+        <img src={homeImg} alt="couple" className="home-border-img" />
+
+    <div className="home-description">
+        <h1 className="home-text">Life-style photographer
+        <br/> based in Sanger, CA</h1>
     </div>
+    <a href='/gallery'><button className="home-button">View Gallery</button></a>
+    
+
+     </div>
+
+
+    
   );
 }
--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./nav.css";
 
 export default function Nav() {
