83a7b4293826cccf36eb5e7f75ab825cdca39420
Kia Moua <116847348+mouakiang@users.noreply.github.com>
2023-12-04 15:14:40 -0500

    added nav and home files

---
+++ src/components/nav/nav.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './nav.css'  
+
+export default function Nav() {
+
+    return (
+        <div className="nav">
+            <h1>Nav</h1>
+        </div>
+    );
+}
