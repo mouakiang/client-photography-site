1e4287a543faf53e19b43076a95211b997515ce8
Kia Moua <116847348+mouakiang@users.noreply.github.com>
2023-12-04 15:48:45 -0500

    header and nav links working and styled

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,12 +1,10 @@
-import React from 'react';
-import './home.css';
+import React from "react";
+import "./home.css";
 
 export default function Home() {
-
-    return (
-        <div className="home">
-            <h1>Home</h1>
-        </div>
-    );
+  return (
+    <div className="home">
+     
+    </div>
+  );
 }
-
--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -1,11 +1,29 @@
 import React from "react";
-import './nav.css'  
+import "./nav.css";
 
 export default function Nav() {
-
-    return (
-        <div className="nav">
-            <h1>Nav</h1>
+  return (
+    <div className="header-nav">
+      <div className="header-announcement-bar-wrapper">
+        <div className="header-title-text">
+          <a id="site-title" href="/">
+            K Jane Moua Photography
+          </a>
         </div>
-    );
+      </div>
+      <div className="header-nav-wrapper">
+        <nav className="header-nav-list">
+          <div className="header-nav-item">
+            <a href="/gallery">Gallery</a>
+          </div>
+          <div className="header-nav-item">
+            <a href="/about">About</a>
+          </div>
+          <div className="header-nav-item">
+            <a href="/bookings">Bookings</a>
+          </div>
+        </nav>
+      </div>
+    </div>
+  );
 }
