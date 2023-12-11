4b590ec98bffeeb775ee9abf58c703dd6fe680d9
Kia Moua <116847348+mouakiang@users.noreply.github.com>
2023-12-11 12:33:16 -0500

    added gallery links and styling for basis

--- src/components/gallery/gallery.jsx
+++ src/components/gallery/gallery.jsx
@@ -1,11 +1,37 @@
 import React from "react";
-import './gallery.css';
+import "./gallery.css";
 
 export default function Gallery() {
-
-    return (
-        <div className="gallery">
-            <h1>Gallery</h1>
+  return (
+    <div className="gallery">
+      <div className="gallery-content-wrapper">
+        <div className="gallery-content-links">
+          <a
+            className="galleryLink"
+            href="https://kjanemouaphotography.shootproof.com/gallery/23452315"
+          >
+            Weddings
+          </a>
+          <a
+            className="galleryLink"
+            href="https://kjanemouaphotography.shootproof.com/gallery/23452336"
+          >
+            Engagements
+          </a>
+          <a
+            className="galleryLink"
+            href="https://kjanemouaphotography.shootproof.com/gallery/23452343"
+          >
+            Family Portraits
+          </a>
+          <a
+            className="galleryLink"
+            href="https://kjanemouaphotography.shootproof.com/gallery/23452321"
+          >
+            Portraits
+          </a>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
