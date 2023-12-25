bb92626fb69882e029a63f058a66e763865be0be
Kia Moua <116847348+mouakiang@users.noreply.github.com>
2023-12-25 18:53:46 -0500

    changes

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -4,7 +4,7 @@ import "./footer.css";
 export default function Footer() {
   return (
     <div className="footer">
-      <h1>Footer</h1>
+      <h1>K Jane Moua Photography</h1>
     </div>
   );
 }
